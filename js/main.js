83187543e5c6b585e1bcb3c8ce627a46eb12c1a8
artistry-intelligence-solutions <info@artistryintelligencesolutions.com>
2020-05-29 20:29:56 +0200

    #04 - dev

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const video = document.getElementById('video');
 
 Promise.all([
+    faceapi.nets.ageGenderNet.loadFromUri('./../js/models/'),
     faceapi.nets.tinyFaceDetector.loadFromUri('./../js/models/'),
     faceapi.nets.faceLandmark68Net.loadFromUri('./../js/models/'),
     faceapi.nets.faceRecognitionNet.loadFromUri('./../js/models/'),
@@ -24,12 +25,52 @@ video.addEventListener('play', () => {
     };
     faceapi.matchDimensions(canvas, displaySize);
     setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-        console.log(detections);
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions()
+        ).withFaceLandmarks().withFaceExpressions().withAgeAndGender().withFaceDescriptors();
+        const results = JSON.stringify(detections);
+        console.log(`age: ${Math.round(detections[0].age)}`);
+        console.log(`gender: ${detections[0].gender}`);
+        if ((Math.round(detections[0].expressions.angry * 10) / 10) > 0.5) {
+            console.log(`disgusted: ${Math.round(detections[0].expressions.angry * 10) / 10}`);
+        } else if ((Math.round(detections[0].expressions.disgusted * 10) / 10) > 0.5) {
+            console.log(`disgusted: ${Math.round(detections[0].expressions.disgusted * 10) / 10}`);
+        } else if ((Math.round(detections[0].expressions.fearful * 10) / 10) > 0.5) {
+            console.log(`fearful: ${Math.round(detections[0].expressions.fearful * 10) / 10}`);
+        } else if ((Math.round(detections[0].expressions.happy * 10) / 10) > 0.5) {
+            console.log(`happy: ${Math.round(detections[0].expressions.happy * 10) / 10}`);
+        } else if ((Math.round(detections[0].expressions.neutral * 10) / 10) > 0.5) {
+            console.log(`neutral: ${Math.round(detections[0].expressions.neutral * 10) / 10}`);
+        } else if ((Math.round(detections[0].expressions.sad * 10) / 10) > 0.5) {
+            console.log(`sad: ${Math.round(detections[0].expressions.sad * 10) / 10}`);
+        } else if ((Math.round(detections[0].expressions.surprised * 10) / 10) > 0.5) {
+            console.log(`surprised: ${Math.round(detections[0].expressions.surprised * 10) / 10}`);
+        }
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
         faceapi.draw.drawDetections(canvas, resizedDetections);
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
         faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        resizedDetections.forEach( detection => {
+          const box = detection.detection.box;
+          const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age) + " year old " + detection.gender });
+          drawBox.draw(canvas);
+        });
     }, 100);
 });
+
+const loading = document.getElementById('loading');
+const testResults = document.getElementById('testResults');
+
+function getMathRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+setTimeout(() => {
+    loading.style.display = 'none';
+    if (getMathRandom(0, 10) >= 5) {
+        testResults.innerText = 'Start running replicant!';
+    } else if (getMathRandom(0, 10) < 5) {
+        testResults.innerText = 'as you were hooman...';
+    }
+}, 3000);
